c6c7f66f45c322530d6ddbe03145e2daf45cf868
Daniel Penalva <dkajah@gmail.com>
2022-08-23 08:11:58 -0300

    fake events kinesis firehose ingestion

---
+++ arquitetura_modulo_2/fake_events.py
@@ -0,0 +1,22 @@
+from boto3 import Session
+import boto3
+import json
+from fake_web_events import Simulation
+
+boto_sess = Session(profile_name='awsadm')
+client = boto_sess.client('firehose',region_name='sa-east-1')
+
+def put_record(event: dict):
+    data = json.dumps(event) + "\n"
+    response = client.put_record(
+            DeliveryStreamName='demo_ing_dados',
+            Record={"Data":data}
+            )
+    print(event)
+    return response
+
+simulation = Simulation(user_pool_size=100,sessions_per_day=100000)
+events = simulation.run(duration_seconds=3000)
+
+for event in events:
+    put_record(event)
